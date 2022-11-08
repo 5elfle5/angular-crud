5048233e987e8b2e4f60a90304a184c5733f9344
Andrii Kapinos <andy@everlend.fi>
2022-11-08 12:29:05 +0200

    back end auth

--- crud-be/src/app.module.ts
+++ crud-be/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { CreateModule } from './modules/create/create.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './core/database/database.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     CreateModule,
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
