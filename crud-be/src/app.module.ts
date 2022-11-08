a06139143057029dd75e8590f7449308b57213d5
Andrii Kapinos <andy@everlend.fi>
2022-11-08 11:20:48 +0200

    users module

--- crud-be/src/app.module.ts
+++ crud-be/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CreateModule } from './create/create.module';
+import { CreateModule } from './modules/create/create.module';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './core/database/database.module';
 
 @Module({
   imports: [
     CreateModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
